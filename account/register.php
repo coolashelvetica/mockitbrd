0717a6f00e8c75e74a18aca436c146420045ec0d
Fara Ashiru <fashiru@imagequix.com>
2013-07-05 12:39:13 -0400

    added switch statements to toggle selected radios

--- account/register.php
+++ account/register.php
@@ -181,14 +181,13 @@ if(!empty($_POST))
             		<div class="register-role">
 				    	<input type="radio" name="role" class="interviewer" value="Interviewer"/>
 					    <label for="interviewer">
-					    	<img class="unswitch" src="/lib/img/intwhite.png"/> 
+					    	<img src="/lib/img/intwhite.png"/> 
 					    	<img class="switch" src="/lib/img/intblue.png"/> 
 					    </label>
 					    <input type="radio" name="role" class="Candidate" value="Candidate"/>
 					    <label for="happy">
-					    	<img class="unswitch" src="/lib/img/candwhite.png"/>
-					    	<img class="switch" src="/lib/img/candblue.png"/>
-					    </label>
+					    	<img src="/lib/img/candwhite.png"/>
+					    	<img class="switch" src="/lib/img/candblue.png"/></label>
 					</div>
             	</div>
             </div>
