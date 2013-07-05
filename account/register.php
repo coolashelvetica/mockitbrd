1fb7591053d34b93a9c55c08d37ed6705acb54c8
Fara Ashiru <fashiru@imagequix.com>
2013-07-05 12:59:03 -0400

    render

--- account/register.php
+++ account/register.php
@@ -182,12 +182,10 @@ if(!empty($_POST))
 				    	<input type="radio" name="role" class="interviewer" value="Interviewer"/>
 					    <label for="interviewer">
 					    	<img src="/lib/img/intwhite.png"/> 
-					    	<img class="switch" src="/lib/img/intblue.png"/> 
 					    </label>
 					    <input type="radio" name="role" class="Candidate" value="Candidate"/>
 					    <label for="happy">
 					    	<img src="/lib/img/candwhite.png"/>
-					    	<img class="switch" src="/lib/img/candblue.png"/></label>
 					</div>
             	</div>
             </div>
